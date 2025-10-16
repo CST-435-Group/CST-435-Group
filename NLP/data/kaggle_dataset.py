9e837a44c6e7cd8004ebc91de7fb965241fe7dbf
Susana016 <shernandez226@my.gcu.edu>
2025-10-16 10:01:22 -0700

    Added NLP App and Dataset

---
+++ NLP/data/kaggle_dataset.py
@@ -0,0 +1,5 @@
+import kaggle
+kaggle.api.dataset_download_files('junaid6731/hospital-reviews-dataset', 
+                                   path='./data', unzip=True)
+
+print("Dataset downloaded and extracted successfully.")
