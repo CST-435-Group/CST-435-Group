c705a8ab85939e19a90efc89b1c9ce7460d87fe9
TheBeestyBoy <livery.gambler-7y@icloud.com>
2025-11-24 10:49:27 -0800

    fixed launcher frontend Home being to skinny on some screens.

--- launcher/frontend/src/pages/Home.jsx
+++ launcher/frontend/src/pages/Home.jsx
@@ -107,7 +107,7 @@ export default function Home() {
       </div>
 
       {/* Project Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-6 max-w-7xl mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl mx-auto">
         {projects.map((project) => {
           const Icon = project.icon
           return (
